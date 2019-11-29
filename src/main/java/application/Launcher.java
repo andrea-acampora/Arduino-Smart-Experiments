5dba18c549b17ccabba6a0c166269d2251bce2ea
Danilo Pianini <danilo.pianini@unibo.it>
2019-11-29 16:43:13 +0100

    Initialize plain Gradle/kts + JavaFX template project

---
+++ src/main/java/application/Launcher.java
@@ -0,0 +1,16 @@
+package application;
+
+/**
+ *
+ */
+public final class Launcher {
+
+    private Launcher() { }
+
+    /**
+     * @param args unused
+     */
+    public static void main(final String[] args) {
+        Main.main(args);
+    }
+}
